90a067b3816011ee30d3e5802c507fae7bdbb9d7
Jan Christensen <79102705+CombustibleMel0n@users.noreply.github.com>
2021-02-25 11:56:06 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -3,29 +3,50 @@ import random
 def main_loop():
     combi =[]
     for i in range(5):
-        combi.append(random.randrange(1,9))
+        combi.append(random.randrange(1,9)) #Random Number
     nbre_essais = 12
     while nbre_essais > 0:
-        essai = input("Rentrez une série de 5 nombres de 1 à 8 : ")
+        essai = input("Enter a series of numbers from 1 to 8 : ")
+        number_spot = 0
+        just_number = 0
         try:
             essai = int(essai)
             if essai >= 11111 and essai <= 88888 :
                 combi_joueur = [int(x) for x in str(essai)]
                 if combi_joueur == combi :
-                    print("Bravo ! Vous avez trouvé la combinaison !")
-                    return
-                else : 
-                    for i in range(5):
-                        if combi_joueur[i] == combi[i]:
-                            print(True)
-                        else :
-                            print(False)
-                    nbre_essais -= 1
-                    print(f"Il vous reste {nbre_essais} essais !")
-            else :
-                print ("Merci de rentrez une série de 5 nombres compris entre 1 et 8: ")
-        except ValueError:
-            print ("Merci de rentrez une série de 5 nombres compris entre 1 et 8: ")
-    print(f"Dommage, la combinaison était {combi} !")
+                    print("Well done ! You found the solution !")
+                    if input('If you want to play again, type \'YES\', otherwise type anything else') == 'YES' :
+                        main_loop()
+                    else :
+                        return
 
+                else :
+                    done = combi[:]
+                    for i in range(5):
+                        for j in range(5):
+                            if i==j and combi_joueur[i] == done[j]:
+                                number_spot +=1
+                                done[j] = 0
+                                break
+                    for i in range(5):
+                        for j in range(5):
+                            if i != j and combi_joueur[i] == done[j]:
+                                just_number += 1
+                                done[j] = 0
+                                break
+                    nbre_essais -= 1
+                    print(f"You have {number_spot} numbers in the right spot")
+                    print(f"You have {just_number} numbers in the wrong spot")
+                    print(f"You have {nbre_essais} remaining !")
+            else :
+                print ("Please enter a series of exactly 5 numbers between 1 and 8 :")
+        except ValueError:
+            print ("Please enter a series of 5 numbers between 1 and 8 : ")
+    print(f"Nice try, the combination was {combi} !")
+    if input('If you want to play again, type \'YES\', otherwise type anything else') == 'YES' :
+        main_loop()
+    else :
+        return
 main_loop()
+
+
